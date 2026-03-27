37b8465733d2ecebe7e07d84230e872e0f3d1369
Joshua-Wright76 <jwrightbluj@gmail.com>
2026-03-27 16:00:17 -0700

    Update to v0.2.0

--- Formula/sitmon.rb
+++ Formula/sitmon.rb
@@ -6,20 +6,12 @@ class Sitmon < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Joshua-Wright76/sitmontui/releases/download/v0.1.0/sitmon_cli"
-      sha256 "2ac73ff1271165051e228242c71c2b46ccb6cd0bd74b3bc8bde7c12bc9771149"
-    end
-    on_intel do
-      url "https://github.com/Joshua-Wright76/sitmontui/releases/download/v0.1.0/sitmon_cli-x86_64-apple-darwin"
-      sha256 "530eb6394db724040d294ff77ff08ece43e7c8325922fa53ecbf093ee5802437"
+      url "https://github.com/Joshua-Wright76/sitmontui/releases/download/v0.2.0/sitmon_cli"
+      sha256 "69f69058d009b88e8a4fa23fe5b2b47387b9d78d4e472a79d4155e464ff66f5d"
     end
   end
 
   def install
     bin.install "sitmon_cli" => "sitmon"
-  end
-
-  test do
-    system "#{bin}/sitmon", "--help"
   end
 end

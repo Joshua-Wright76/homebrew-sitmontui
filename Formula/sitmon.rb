274d7a3ceba23e5021e6c6d969219a185f8fd5b8
Joshua-Wright76 <jwrightbluj@gmail.com>
2026-04-06 12:38:10 -0700

    new releasec

--- Formula/sitmon.rb
+++ Formula/sitmon.rb
@@ -6,8 +6,8 @@ class Sitmon < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Joshua-Wright76/sitmontui/releases/download/v1.2.0/sitmon_cli"
-      sha256 "334947ebd324ccc818c8d8e2855b273ac9781fc3a54c88e6492bb56d35e2777d"
+      url "https://github.com/Joshua-Wright76/sitmontui/releases/download/v1.4.0/sitmon_cli"
+      sha256 "3d22a58aa5d96856058786be318f9dbca7553d0a9b19e301c5969c16781f5"
     end
   end
 
